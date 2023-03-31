06bacd25488b1b598a0cbbef9b92f6faba7aa1cd
Ramanathan <c.ramanathan23@gmail.com>
2023-03-31 15:45:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   const thresholdPct = core.getInput('threshold-pct');
   console.log(`Threshold Percentage - ${thresholdPct}%`);
-  const data = require('./coverage/coverage-summary.json');
+  const data = require('/coverage/coverage-summary.json');
   core.setOutput("status", data.total.lines.pct >= thresholdPct);
 } catch (error) {
   core.setFailed(error.message);
